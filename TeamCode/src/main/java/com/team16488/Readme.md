1543a84a8ecd5e64a90c8ffd1253ea6e1228ad6a
Parham-Baghbanbashi204 <parham.baghbanbashi204@gmail.com>
2020-11-30 23:36:18 -0500

    created initial file system

---
+++ TeamCode/src/main/java/com/team16488/Readme.md
@@ -0,0 +1,7 @@
+#File structure explanation
+com/ 
+    team16488/
+        autonomous/ # In this folder are separate autonomous modes
+        components/ # Different components of the robot such as drive, shooter, intake, etc
+        common/ # Parts of the robot that belong to all parts, such as drivers for sensors.
+        robot.java # Main Robot File
---
+++ TeamCode/src/main/java/com/team16488/robot.java
@@ -0,0 +1,20 @@
+package com.team16488;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * This is the main OpMode for the driver operated portion of the event
+ * This code will either call commands on button presses, run the OI code which will handle the commands
+ * or have all of the logic for the control inputs done here IDK yet
+ */
+public class robot extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
